b2718dedd9ffaf2944488ea373b22625b5323b3e
viriato <viriatobonifa22@gmail.com>
2025-01-19 20:58:39 +0200

    Atualizações do site4

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,4 @@
-import { HashRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
-
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Clientes from "./pages/servicos/Clientes/Clientes";
@@ -14,28 +13,26 @@ import RegistarMortalidade from "./pages/servicos/Mortalidades/registMortalidade
 import MortalidadeView from "./pages/servicos/Mortalidades/mortalidadeView";
 
 export default function AppRouter() {
-    return (
-    
-          <HashRouter>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/registar-clientes/:id" element={<Clientes />} />
-                <Route path="/registarclientes" element={<Clientes />} />
-                <Route path="/clientesview" element={<ClientesView />} />
-                <Route path="/RegistarStock" element={<RegistarStock />} />
-                <Route path="/registar-stock/:id" element={<RegistarStock />} />
-                <Route path="/stockview" element={<StockView />} />
-                <Route path="/registarmercadoria" element={<RegistarMercadoria />} />
-                <Route path="/registar-mercadoria/:id" element={<RegistarMercadoria />} />
-                <Route path="/mercadoriaview" element={<MercadoriaView />} />
-                <Route path="/registarvenda" element={<RegistarVenda />} />
-                <Route path="/registar-venda/:id" element={<RegistarVenda />} />
-                <Route path="/vendasview" element={<VendasView />} />
-                <Route path="/registarmortalidade" element={<RegistarMortalidade />} />
-                <Route path="/registar-mortalidade/:id" element={<RegistarMortalidade />} />
-                <Route path="/mortalidadeview" element={<MortalidadeView />} />
-            </Routes>
-        </HashRouter>
-       
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/registar-clientes/:id" element={<Clientes />} />
+        <Route path="/registarclientes" element={<Clientes />} />
+        <Route path="/clientesview" element={<ClientesView />} />
+        <Route path="/RegistarStock" element={<RegistarStock />} />
+        <Route path="/registar-stock/:id" element={<RegistarStock />} />
+        <Route path="/stockview" element={<StockView />} />
+        <Route path="/registarmercadoria" element={<RegistarMercadoria />} />
+        <Route path="/registar-mercadoria/:id" element={<RegistarMercadoria />} />
+        <Route path="/mercadoriaview" element={<MercadoriaView />} />
+        <Route path="/registarvenda" element={<RegistarVenda />} />
+        <Route path="/registar-venda/:id" element={<RegistarVenda />} />
+        <Route path="/vendasview" element={<VendasView />} />
+        <Route path="/registarmortalidade" element={<RegistarMortalidade />} />
+        <Route path="/registar-mortalidade/:id" element={<RegistarMortalidade />} />
+        <Route path="/mortalidadeview" element={<MortalidadeView />} />
+      </Routes>
+    </Router>
+  );
 }
